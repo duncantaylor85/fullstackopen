be9c02c65ab2f489a90b5d9f1aa98f547b88762b
duncantaylor85 <duncantaylor@mailbox.org>
2023-02-25 15:08:34 +0100

    changes App

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,8 @@
 const App = () => {
-  console.log('Hello from component')
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
   return (
     <div>
       <p>Hello World</p>
