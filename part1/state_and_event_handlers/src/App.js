d1fb3bd421a91206b0f1075ba9ce3611e8102b84
duncantaylor85 <duncantaylor@mailbox.org>
2023-02-27 11:52:48 +0100

    adds counter and react state hook

--- part1/state_and_event_handlers/src/App.js
+++ part1/state_and_event_handlers/src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 
-function App() {
-  return(<div>
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  setTimeout(() => {
+    setCounter(counter + 1)
+  }, 1000)
 
-  </div>)
+  console.log("rendering...", counter)
+
+  return(
+    <div>
+      {counter}
+    </div>
+  )
 }
 
 export default App;
