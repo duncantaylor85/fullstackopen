9b9ea12ae89a1fb21aa44046ad5c54242be282f6
duncantaylor85 <duncantaylor@mailbox.org>
2023-02-27 20:45:19 +0100

    adds Button and Display components

--- part1/state_and_event_handlers/src/App.js
+++ part1/state_and_event_handlers/src/App.js
@@ -1,21 +1,31 @@
 import { useState } from "react";
 
+const Display = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>{props.name}</button>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
   
 
   return(
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} name={"plus"} />
+      <Button onClick={decreaseByOne} name={"minus"} />
+      <Button onClick={setToZero} name={"zero"} />
     </div>
   )
 }
