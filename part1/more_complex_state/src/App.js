78552c4a2f2a71822b420dfeafee5b44b613ca6b
duncantaylor85 <duncantaylor@mailbox.org>
2023-02-28 11:38:44 +0100

    adds array state

--- part1/more_complex_state/src/App.js
+++ part1/more_complex_state/src/App.js
@@ -1,14 +1,32 @@
 import {useState} from 'react'
 
 function App() {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setClicks({
+      ... clicks,
+      left: clicks.left + 1,
+    })
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setClicks({
+      ... clicks,
+      right: clicks.right +1
+    })
+  }
   return(
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
-      {right}
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   )  
 }
