61ed32f3651256380c0c1fff0198379439c3e125
duncantaylor85 <duncantaylor@mailbox.org>
2023-03-01 12:31:45 +0100

    adds function that returns a function

--- part1/more_complex_state/src/App.js
+++ part1/more_complex_state/src/App.js
@@ -1,54 +1,15 @@
 import {useState} from 'react'
 
-const History = (props) => {
-  if(props.total === 0) {
-  return(
-      <div>the app is used by pressing the buttons</div>
-  )
-  }
-  return(
-    <div>{props.total}</div>
-  )
-}
-
-const Button = ({handleClick, text}) => {
-  return(
-    <button onClick={handleClick}>{text}</button>
-  )
-}
 
 function App() {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
+  const [value, setValue] = useState(10)
+  const hello = (who) => () => console.log("Hello", who)
+    
      
-    const updatedLeft = clicks.left + 1
-    setClicks({
-      ... clicks,
-      left: updatedLeft,
-    })
-    setTotal(updatedLeft + clicks.right)
-  }
-  const handleRightClick = () => {
-    const updatedRight = clicks.right + 1
-    setClicks({
-      ... clicks,
-      right: updatedRight 
-    })
-    setTotal(clicks.left + updatedRight)
-  }
-  
   return(
     <div>
-      {clicks.left}
-      <Button handleClick={handleLeftClick} text={"left"} />
-      <Button handleClick={handleRightClick} text={"right"} />
-      {clicks.right}
-      <History total={total} />
+      <button onClick={hello("world")}>world</button>
+      <button onClick={hello("react")}>react</button>
     </div>
   )  
 }
