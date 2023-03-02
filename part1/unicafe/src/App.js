591a1f791ba715ab1b738737b42e4517c7c8127c
duncantaylor85 <duncantaylor@mailbox.org>
2023-03-02 12:57:18 +0100

    completes step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,33 @@
 import { useState } from 'react'
-
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+const Statistics = (props) => {
+  return(
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
 const App = () => {
   const [good, setGood] = useState(0) 
   const [neutral, setNeutral] = useState(0) 
   const [bad, setBad] = useState(0) 
+
   return(
     <div>
-      
+      <h1>give feedback</h1> 
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
