3403b9820ce10ba2aec3325d6495263db7e67ac7
Duncan Taylor <duncantaylor@mailbox.org>
2023-03-14 17:47:40 +0100

    refactors Course into separate module

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,11 +1,9 @@
 import Part from './Part'
 
-const Content = (props) => {
+const Content = ({parts}) => {
 	return (
 		<div>
-			<Part part={props.parts[0]} />
-			<Part part={props.parts[1]} />
-			<Part part={props.parts[2]} />
+            {parts.map(part => <Part key={part.id} part={part} />)}
 		</div>
 	);
 };
--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,24 +2,7 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = () => {
-	const course = {
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-			},
-		],
-	};
+const Course = ({course}) => {
 
 	return (
 		<div>
