67c69f472d2cb124182a112121ee717c897f82a8
Duncan Taylor <duncantaylor@mailbox.org>
2023-04-04 13:57:55 +0200

    adds source of notes data as server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Name from "./components/Name";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
+  const addName = (event) => {
+    event.preventDefault();
+    const personFound = persons.find((person) => person.name === newName);
+    if (personFound) {
+      alert(`${newName} was already added to the phonebook`);
+    } else {
+      const nameObject = { name: newName, number: newNumber };
+      setPersons(persons.concat(nameObject));
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <div>
+        filter shown with
+        <input />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul style={{ padding: 0, listStyleType: "none" }}>
+        {persons.map((person) => (
+          <Name key={person.name} name={person.name} />
+        ))}
+      </ul>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
---
+++ part2/phonebook/src/components/Name.js
@@ -0,0 +1,7 @@
+const Name = ({name}) => {
+    return(
+        <li>{name}</li>
+    )
+}
+
+export default Name
