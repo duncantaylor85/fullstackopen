268ebee42e1867c038ed00887d76570833e12df2
Duncan Taylor <duncantaylor@mailbox.org>
2023-04-13 11:15:28 +0200

    refactors server communication. Now is separate component

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,14 +1,14 @@
 import Note from "./components/Note";
 import { useState, useEffect } from "react";
-import axios from "axios";
-
+import noteService from './services/notes'
 const App = (props) => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
+    noteService.getAll()
+    .then((response) => {
       setNotes(response.data);
     });
   }, []);
@@ -24,11 +24,24 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    
+      noteService.create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+    })
   };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((note) => note.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService.update(id, changedNote)
+    .then(response => {
+      setNotes(notes.map((note) => note.id !== id ? note: changedNote))
+    })
+  }
 
   return (
     <div>
@@ -44,7 +57,7 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} content={note.content} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         ))}
       </ul>
       <form onSubmit={addNote}>
--- part2/notes/src/components/Note.js
+++ part2/notes/src/components/Note.js
@@ -1,6 +1,10 @@
-const Note = ({ content }) => {
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? 'make not important' : 'make important';
   return (
-    <li>{content}</li>
+    <div>
+      <li>{note.content}</li>
+      <button onClick={toggleImportance}>{label}</button>
+    </div>
   )
 }
 
